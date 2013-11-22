e53d2de69645c2ceeb87637cf74830c806993751
curriedav <curriedav@gmail.com>
2013-11-22 10:48:58 -0800

    problem solutions

--- 3.js
+++ 3.js
@@ -3,11 +3,12 @@
 // fix it to log out 12!
 
 function square(num) {
-  result = num * num
-  return result
+  	var result = num * num;
+  	return result;
 }
 
-result = square(4)
-result2 = square(2)
-var subtracted = result - result2
-console.log(subtracted)
+var result = square(4),
+	result2 = square(2),
+	subtracted = result - result2;
+
+console.log(subtracted);
--- 4.js
+++ 4.js
@@ -2,18 +2,19 @@
 // even though it should only add even numbers
 // fix it to only add even numbers!
 
-var evenNumbers = []
+var evenNumbers = [];
 
 function addEvensOnly(num) {
-  if (num % 2 === 0)
-    console.log(num, 'is even, adding!')
-    evenNumbers.push(num)
+  	if (num % 2 === 0) {
+    	console.log(num, 'is even, adding!');
+    	evenNumbers.push(num);
+	}
 }
 
-addEvensOnly(0)
-addEvensOnly(1)
-addEvensOnly(2)
-addEvensOnly(3)
-addEvensOnly(4)
+addEvensOnly(0);
+addEvensOnly(1);
+addEvensOnly(2);
+addEvensOnly(3);
+addEvensOnly(4);
 
-console.log('even numbers: ', evenNumbers)
+console.log('even numbers: ', evenNumbers);
